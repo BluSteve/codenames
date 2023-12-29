91ce937875f0f6748ea90f9dbe44fa9f8bb48cbf
BluSteve <billiegatesftw@gmail.com>
2023-12-29 20:24:29 +0800

    Bug fixes + output hint count

--- nishy.py
+++ nishy.py
@@ -5,20 +5,25 @@ from nltk.metrics.distance import edit_distance
 
 word2vec_sample = str(find('models/word2vec_sample/pruned.word2vec.txt'))
 model = gensim.models.KeyedVectors.load_word2vec_format(word2vec_sample, binary=False)
-
+SAMENESS_THRESHOLD = 0.5
+GOOD_THRESHOLD = 0.1
+EMPHASIS_FACTOR = 2
 
 class NishyBot:
     def __init__(self, good, bad, okay, assassin):
-        self.good = good;
-        self.bad = bad;
-        self.okay = okay;
-        self.assassin = assassin;
+        self.good = good
+        self.bad = bad
+        self.okay = okay
+        self.assassin = assassin
 
     def get_similar_hints(self):
         combined = []
         for word in self.good:
-            for hint in self.filter_hints([x[0] for x in model.most_similar(positive=[word], topn=10)]):
-                combined.append(hint)
+            try:
+                for hint in self.filter_hints([x[0] for x in model.most_similar(positive=[word], topn=10)]):
+                    combined.append(hint)
+            except KeyError:
+                pass
         return combined
 
     def filter_hints(self, hints):
@@ -26,10 +31,9 @@ class NishyBot:
         for word in self.good:
             leven = [edit_distance(x, word) for x in hints]
 
-            SAMENESS_THRESHOLD = 0.7
             for i in range(len(hints)):
-                if (leven[i] / len(word) < SAMENESS_THRESHOLD):
-                    barray[i] = False;
+                if leven[i] / len(word) < SAMENESS_THRESHOLD:
+                    barray[i] = False
 
         final_hints = [x for i, x in enumerate(hints) if barray[i]]
         return final_hints
@@ -43,24 +47,39 @@ class NishyBot:
         return google_words
 
     def to_synsets(words):
-        return [wn.synsets(x)[0] for x in words]
+        r = []
+        for x in words:
+            synsets = wn.synsets(x)
+            if len(synsets) > 0:
+                r.append(synsets[0])
+
+        return r
 
     def get_hints(self):
-        similar_hints = self.get_similar_hints()
 
         good_synsets = NishyBot.to_synsets(self.good)
         bad_synsets = NishyBot.to_synsets(self.bad)
         okay_synsets = NishyBot.to_synsets(self.okay)
         assassin_synsets = NishyBot.to_synsets(self.assassin)
-        hints_synsets = NishyBot.to_synsets(similar_hints)
+
+
+        hints_synsets = []
+        similar_hints = []
+        for word in self.get_similar_hints():
+            synsets = wn.synsets(word)
+            if len(synsets) > 0:
+                hints_synsets.append(synsets[0])
+                similar_hints.append(word)
 
         google_synsets = []
         final_google_words = []
         for word in self.get_google_words():
             synsets = wn.synsets(word)
-            if (len(synsets) > 0):
+            if len(synsets) > 0:
                 google_synsets.append(synsets[0])
                 final_google_words.append(word)
+
+
         google_synsets = hints_synsets + google_synsets
         final_google_words = similar_hints + final_google_words
 
@@ -68,34 +87,40 @@ class NishyBot:
 
         for i, x in enumerate(google_synsets):
             good_sum = 0
+            good_count = 0
             for y in good_synsets:
-                good_sum += wn.path_similarity(x, y) ** 2
+                ps = wn.path_similarity(x, y)
+                good_sum += ps ** EMPHASIS_FACTOR
+                if ps > GOOD_THRESHOLD:
+                    if final_google_words[i] == 'happening':
+                        print(y)
+                    good_count += 1
             bad_sum = 0
             for y in bad_synsets:
-                bad_sum += wn.path_similarity(x, y) ** 2
+                bad_sum += wn.path_similarity(x, y) ** EMPHASIS_FACTOR
             okay_sum = 0
             for y in okay_synsets:
-                okay_sum += wn.path_similarity(x, y) ** 2
+                okay_sum += wn.path_similarity(x, y) ** EMPHASIS_FACTOR
             assassin_sum = 0
             for y in assassin_synsets:
-                assassin_sum += wn.path_similarity(x, y) ** 2
+                assassin_sum += wn.path_similarity(x, y) ** EMPHASIS_FACTOR
 
             index = good_sum * 2 - bad_sum * 2 - okay_sum - assassin_sum * 5
 
-            to_sort.append((final_google_words[i], x, index))
+            to_sort.append((final_google_words[i], x, good_count, index))
 
             # print(x, index, good_sum, bad_sum, okay_sum, assassin_sum)
 
-        to_sort.sort(key=lambda x: x[2], reverse=True)
+        to_sort.sort(key=lambda x: x[-1], reverse=True)
 
         dict = {}
         for x in to_sort[::-1]:
             dict[x[1]] = x
 
         vc = list(dict.values()).copy()
-        vc.sort(key=lambda x: x[2], reverse=True)
+        vc.sort(key=lambda x: x[-1], reverse=True)
 
-        to_give = [x[0] for x in vc]
+        to_give = [x[0] + ' ' + str(x[2]) for x in vc]
         return to_give[:10]
 
 
